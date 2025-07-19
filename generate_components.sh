9df5bf158af3190dc9d7c8c2cd560598bda8c893
Konstructor-pro <qqra@mail.ru>
2025-07-19 22:26:35 +0300

    пересобран скрипт generate_components.sh

--- generate_components.sh
+++ generate_components.sh
@@ -1,10 +1,22 @@
-# все команды через консоль
+# Удаляем BOM если присутствует и обрабатываем данные
+tail -c +4 components.csv | tail -n +2 | while IFS='' read -r line; do
+    # Заменяем последовательные разделители на заполнители
+    line=$(echo "$line" | sed 's/;;/;-;/g; s/;;/;-;/g; s/;$/;-/')
+    
+    # Разбиваем строку на поля
+    IFS=';' read -r poz tip name marking quantity date dela why comment <<< "$line"
+    
+    # Восстанавливаем пустые значения
+    for var in dela why comment; do
+        declare -n ref=$var
+        ref=${ref//-/}
+    done
 
-# Обрабатываем данные
-tail -n +2 components.csv | while IFS=';' read -r poz tip name marking quantity date dela why comment; do
+    # Генерируем имя файла
     num=$(printf "%03d" "$poz")
     filename="components/$num.txt"
     
+    # Создаем файл
     cat << EOF > "$filename"
 поз: $poz
 тип компонента: $tip
@@ -16,4 +28,6 @@ tail -n +2 components.csv | while IFS=';' read -r poz tip name marking quantity 
 для чего использовать: $why
 примечание: $comment
 EOF
+
+    echo "Создан файл: $filename"
 done
